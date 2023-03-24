df13f5a50325e6ff17b9e7ac82466daf65a618a2
Vladisalv Dudashvili <dudashvilivladislav@gmail.com>
2023-03-24 11:36:25 +0400

    add new pages

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,17 +1,24 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { routerConfig } from 'shared/config/routeConfig/routeConfig';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
-const AppRouter = () => {
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<Routes>
-				{Object.values(routerConfig).map(({ element, path }) => (
-					<Route key={path} path={path} element={<div className="page-wrapper">{element} </div>} />
-				))}
-			</Routes>
-		</Suspense>
-	);
-};
+const AppRouter = () => (
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
+);
 
 export default AppRouter;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-	const { t, i18n } = useTranslation('main');
-	return <div>{t('mainPage')}</div>;
+    const { t } = useTranslation();
+
+    return (
+        <div>
+            {t('Главная страница')}
+        </div>
+    );
 };
 
 export default MainPage;
